d898283441fc2f650bd01e041ad59e1b71dd8603
Guilherme Chamorro <poltegaisnt@gmail.com>
2025-01-25 20:28:59 -0300

    Creating Project

---
+++ src/test/java/dev/stormgui/pope_api/PopeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.stormgui.pope_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
